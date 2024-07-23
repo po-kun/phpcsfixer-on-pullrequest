66085376722a84818e1614f7a613579cc5b1d068
po-kun <po-kun>
2024-07-24 02:29:49 +0900

    fixed

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -33,7 +33,7 @@ $rules = [
         'single_line' => true,
     ],
     'concat_space' => [
-        'spacing' => 'none',
+        'spacing' => 'one', // custom
     ],
     'constant_case' => ['case' => 'lower'],
     'declare_equal_normalize' => true,
--- index.php
+++ index.php
@@ -6,14 +6,24 @@ use PhpCsFixer\Finder;
 $b = 'cds';
 $d = 'neo';
 
-$a = $b.$d;   // $a = 'cdsneo' !!
-  
-$fmod =         'dsdfs';
+$a = $b . $d;   // $a = 'cdsneo' !!
 
-$pj42 = 'sdad'.'dsljmfds';
+$fmod = 'dsdfs';
+
+$pj42 = 'sdad' . 'dsljmfds';
 
 foreach (range(40, 69) as $m) {
     $fmod .= chr($m);
 }
 
 $infg = [4];
+
+/**
+ * Undocumented function
+ *
+ * @return int
+ */
+function osadm42() :int
+{
+    return 42;
+}
